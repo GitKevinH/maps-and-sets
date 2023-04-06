15fdbd13c14c5122cb555b676545bc5f25c7f84e
Kevin Hoang <kevin.hoang199@gmail.com>
2023-04-05 21:28:53 -0400

    added file

---
+++ mapsAndSets.js
@@ -0,0 +1,27 @@
+// Map porition of assignment
+
+let fruitInventory = new Map;
+
+fruitInventory.set("apple", 10);
+fruitInventory.set("banana", 20);
+fruitInventory.set("orange", 30);
+
+
+
+fruitInventory.set("apple",15)
+
+let bananaInventory = fruitInventory.get("banana")
+
+// Set portion of assignment
+
+let uniqueColors = new Set;
+
+uniqueColors.add("red");
+uniqueColors.add("blue");
+uniqueColors.add("green");
+
+uniqueColors.add("yellow");
+
+let hasBlue=uniqueColors.has("blue");
+
+console.log(hasBlue);
